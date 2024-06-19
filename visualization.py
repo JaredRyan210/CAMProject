1b7f6d75f91b81d9927337bf035ba082c586868b
JRyan109 <ryan109@csusm.edu>
2024-06-18 17:49:59 -0700

    made object a different color

--- visualization.py
+++ visualization.py
@@ -1,13 +1,20 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from stl import mesh
 
-def visualize_toolpath(toolpath, title="Toolpath Visualization"):
+def visualize_toolpath_with_model(model, toolpath, title="Toolpath Visualization"):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
+    for vector in model.vectors:
+        ax.plot([vector[0][0], vector[1][0]], [vector[0][1], vector[1][1]], [vector[0][2], vector[1][2]], color='gray')
+        ax.plot([vector[1][0], vector[2][0]], [vector[1][1], vector[2][1]], [vector[1][2], vector[2][2]], color='gray')
+        ax.plot([vector[2][0], vector[0][0]], [vector[2][1], vector[0][1]], [vector[2][2], vector[0][2]], color='gray')
+
+
     xs, ys, zs = zip(*toolpath)
 
-    ax.plot(xs, ys, zs, label='ToolPath')
+    ax.plot(xs, ys, zs, label='ToolPath', color='blue')
 
     ax.scatter(xs[0], ys[0], zs[0], color='green', s=100, label='Start', marker='o')
     
