cd8b9f4b3ec832cafdffa242c8ed5387154735be
JRyan109 <ryan109@csusm.edu>
2024-06-18 17:32:09 -0700

    added start and end points of toolpath

--- visualization.py
+++ visualization.py
@@ -1,9 +1,21 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def visualize_toolpath(toolpath,):
+def visualize_toolpath(toolpath, title="Toolpath Visualization"):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+
     xs, ys, zs = zip(*toolpath)
-    ax.plot(xs, ys, zs)
+
+    ax.plot(xs, ys, zs, label='ToolPath')
+
+    ax.scatter(xs[0], ys[0], zs[0], color='green', s=100, label='Start', marker='o')
+    
+    ax.scatter(xs[-1], ys[-1], zs[-1], color='red', s=100, label='End', marker='x')
+
+    ax.set_title(title)
+    ax.legend()
+
     plt.show()
+    
+    
